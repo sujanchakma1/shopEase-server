c90114d012816255b273aeb01d992a41bb84549e
sujanchakma1 <sujanckz926@gmail.com>
2025-12-27 21:02:08 +0600

    added popular product & update order count

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3000;
-require("dotenv").config();
 const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
@@ -30,7 +30,7 @@ const VerifyToken = (req, res, next) => {
   });
 };
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ra0uaai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ra0uaai.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -185,6 +185,19 @@ async function run() {
         res.status(500).send({ message: "Error fetching products", error });
       }
     });
+    app.get("/popular-products", async (req, res) => {
+      try {
+        const result = await productsCollection
+          .find()
+          .sort({ orderCount: -1 }) // 🔥 highest first
+          .limit(8)
+          .toArray();
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: "Failed to load popular products" });
+      }
+    });
 
     app.get("/products", async (req, res) => {
       try {
@@ -289,6 +302,15 @@ async function run() {
       try {
         const product = req.body;
         const result = await orderCollection.insertOne(product);
+        // 2️⃣ Increment orderCount in product
+        const updateResult = await productsCollection.updateOne(
+          { _id: new ObjectId(product.productId) },
+          { $inc: { orderCount: 1 } }
+        );
+
+        if (updateResult.matchedCount === 0) {
+          return res.status(404).send({ error: "Product not found" });
+        }
         res.send(result);
       } catch (error) {
         console.log("ordered fetching error");
